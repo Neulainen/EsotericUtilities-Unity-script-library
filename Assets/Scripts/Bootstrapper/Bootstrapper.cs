521762dea0f749a53431cb8f123cbc96fc706f8a
Neulainen <gleoj001@edu.xamk.fi>
2026-04-24 19:03:43 +0300

    Moved bootstrapper and initializable to SceneManagement

--- Assets/Scripts/Bootstrapper/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper/Bootstrapper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace EsotericUtilities
+namespace EsotericUtilities.SceneSystems
 {
     public class Bootstrapper : MonoBehaviour
     {
--- Assets/Scripts/Bootstrapper/IInitializable.cs
+++ Assets/Scripts/Bootstrapper/IInitializable.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-namespace EsotericUtilities
+namespace EsotericUtilities.SceneSystems
 {
     public interface IInitializable
     {
