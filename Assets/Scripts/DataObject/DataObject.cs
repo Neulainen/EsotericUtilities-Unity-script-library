5b289156098531fccefa94edd3594d53f03390b6
Neulainen <gleoj001@edu.xamk.fi>
2026-04-24 19:00:25 +0300

    Added data object, reorganized files

--- Assets/Scripts/DataObject/DataObject.cs
+++ Assets/Scripts/DataObject/DataObject.cs
@@ -6,13 +6,14 @@ namespace EsotericUtilities
 
     public abstract class DataObject<T> : ScriptableObject where T : new()
     {
-        [field: SerializeField] public T Data { get; private set; }
+        [field: SerializeField] public T Data { get; private set; } = new();
+
 #if UNITY_EDITOR
-        private void OnEnable()
+        protected virtual void OnEnable()
         {
             EditorApplication.playModeStateChanged += OnPlaymodeStateChanged;
         }
-        private void OnDisable()
+        protected virtual void OnDisable()
         {
             EditorApplication.playModeStateChanged -= OnPlaymodeStateChanged;
         }
@@ -33,10 +34,16 @@ namespace EsotericUtilities
                 default: { return; }
             }
         }
+        /// <summary>
+        /// Actions to do when we exist the playmode. Resets data object as default.
+        /// </summary>
         protected virtual void OnPlaymodeExit()
         {
             Data = new();
         }
+        /// <summary>
+        /// Actions to do when we enter the playmode. Does nothing as default.
+        /// </summary>
         protected virtual void OnPlaymodeEnter() { }
 #endif
     }
