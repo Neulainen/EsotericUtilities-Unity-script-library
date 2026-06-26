68147dc4158aab56bcb3f12a8ca506c1b5dc78f8
Neulainen <gleoj001@edu.xamk.fi>
2026-06-26 22:04:35 +0300

    Some playing around with input data object

---
+++ Assets/EsotericModules/EsotericPlayerControllers/PlayerInputDataObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using EsotericUtilities;
+namespace EsotericPlayerControllers
+{
+    [CreateAssetMenu(fileName = "PlayerInputDataObject", menuName = "Scriptable Objects/PlayerInputDataObject")]
+    public class PlayerInputDataObject : DataObject<PlayerInputData>
+    {
+    
+    }
+}
